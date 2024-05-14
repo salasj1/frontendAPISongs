e59d768d3613f30e8382198464d27dd3e4812f8e
salasj1 <alejandrosalas0922@gmail.com>
2024-05-14 00:14:06 -0400

    Eliminacion de cosas innecesarias

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async redirects() {
+        return [
+          {
+            source: 'http://localhost:3000/',
+            destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}`,
+            permanent: true,
+          },
+          // puedes agregar más redirecciones aquí
+        ];
+      },
 };
 
 module.exports = nextConfig;
